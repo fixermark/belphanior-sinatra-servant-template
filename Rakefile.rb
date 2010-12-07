1b88011db4a4f4ceb3956b733c7562bf24b5c7f8
Mark T. Tomczak <iam@fixermark.com>
2010-12-07 08:16:02 -0500

    Moved belphanior servant template files into a more formalized location.

--- Rakefile.rb
+++ Rakefile.rb
@@ -3,7 +3,7 @@ require 'rake/testtask'
 
 desc "Run basic tests"
 Rake::TestTask::new "test" do |t|
-  t.pattern = "tc*.rb"
+  t.pattern = "belphanior/servant/tc*.rb"
   t.verbose = true
   t.warning = true
 end
