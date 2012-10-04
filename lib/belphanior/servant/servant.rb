b857a355286d80392f7139ca9a9f85d0ed790071
Mark Tomczak <iam@fixermark.com>
2012-10-04 02:00:48 -0400

    Changed config file setting from flag (which doesn't work without Sinatra::Sugar) to environment variable

--- lib/belphanior/servant/servant.rb
+++ lib/belphanior/servant/servant.rb
@@ -8,28 +8,24 @@ require 'sinatra/base'
 module Sinatra
   module Servant
     def self.registered(app)
-
-      OptionParser.new { |opts|
-        opts.on('-c', '--config-file', 
-                'Specify the configuration file for the servant.') do |file|
-          set :servant_config_file, file
-        end
-      }
+      if ENV.has_key? "BELPHANIOR_CONFIG_FILE"
+        app.set :servant_config_file, ENV["BELPHANIOR_CONFIG_FILE"]
+      end
     end
-    
+
     def servant_init
       load_servant_config
       # readonly because changing them involves rebooting the server, so the
       # change cannot be honored.
       servant_config.set_readonly "bind"
       servant_config.set_readonly "port"
-      
+
       set :bind, servant_config.get("bind")
       set :port, servant_config.get("port")
-      
+
       # To simplify functionality, we make every request handle synchronously.
       enable :lock
-      
+
       # default handler for top-level index. A user-defined top-level index
       # created before servant.init is called would override this.
       get '/' do
@@ -43,12 +39,12 @@ module Sinatra
     <h1>Belphanior Servant Online</h1>
     <h2>#{server_name}</h2>
     <p>Hello! I am #{server_name}, and I am happy to serve you.</p>
-    <p>To learn more about what I can do, check my 
+    <p>To learn more about what I can do, check my
        <a href="/protocol">protocol</a>.</p>
     <p>Want to know my settings? Check my <a href="/config">config</a>.
   </body>
 EOF
-      end  
+      end
     end
   end
   register Servant
